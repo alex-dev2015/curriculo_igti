26c7d9e93448ee7e3905b1c7dd32d16c358ccc05
AlexSousa <alex.sousa20@hotmail.com>
2020-07-15 17:57:12 -0300

    Adicionada novas funcionalidades no form de cadastro

--- js/script.js
+++ js/script.js
@@ -1,3 +1,72 @@
-var title = document.querySelector('h1');
-title.textContent = 'Alex do Nascimento de Sousa';
+window.addEventListener('load', start);
+
+var globalNames = ['Um', 'Dois', 'Três', 'Quatro'];
+var inputName = null;
+
+function start(){
+    inputName = document.querySelector('#name_user');
+    preventFormSubmit();
+    activateInput();
+    render();
+    
+}
+
+function preventFormSubmit(){
+    function handleFormSubmit(event){
+        event.preventDefault();
+        
+    }
+    
+    var form = document.querySelector('form');
+    form.addEventListener('submit', handleFormSubmit);
+}
+
+function activateInput(){
+    function insertName(newName){
+        globalNames.push(newName);
+        render();
+    }
+
+    function handleTyping(event){
+      if(event.key === 'Enter')
+      {
+        insertName(event.target.value);  
+      }
+    }
+
+    inputName.addEventListener('keyup' , handleTyping);
+    inputName.focus();
+}
+
+function render(){
+   var divNames = document.querySelector('#names'); 
+   divNames.innerHTML = ''; 
+  
+   var ul   = document.createElement('ul');
+   
+   for(var i = 0; i < globalNames.length; i++){
+      var currentName = globalNames[i]; 
+      var li = document.createElement('li');
+      
+      var button = document.createElement('button');
+      button.classList.add('deleteButton');
+      button.textContent = 'X';
+      
+      var span = document.createElement('span');
+      span.textContent = currentName;  
+      
+      li.appendChild(button);
+      li.appendChild(span);
+      ul.appendChild(li);
+   }
+   
+ 
+   divNames.appendChild(ul);
+   clearInput();
+}
+
+function clearInput(){
+   inputName.value = '';
+   inputName.focus(); 
+}
 
